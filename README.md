b8214f1581bdaf4661492c5a3ae4240dbe249d2b
Noel Delgado <auhcsei@gmail.com>
2013-10-04 00:24:27 -0500

    01001

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-## hex-rgb-hsl color converter
+## Color Converter
 
-Build using [Values.js](https://github.com/noeldelgado/Values.js).
-
+Convert HEX/RGB/HSL color formats.
 http://noeldelgado.github.io/color-converter/
+
+This micro tool was built using [Values.js](https://github.com/noeldelgado/Values.js).
--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -2,6 +2,7 @@
 
     "use strict";
 
+    // instance of Values.js
     global.Color = new Values('rgb(255, 0, 0)');
 
     var colorFormats = {
@@ -9,78 +10,70 @@
             hex: document.querySelector('.js-hex'),
             rgb: document.querySelector('.js-rgb'),
             hsl: document.querySelector('.js-hsl'),
-            wrapper: document.querySelector('.wrapper'),
+            wrapper: document.querySelector('.main-container--inner'),
             links: document.querySelectorAll('.footer a'),
             body: document.body
         },
 
-        init : function() {
+        init : function init() {
             this.UI._links = [].slice.call(this.UI.links, 0);
             this.bindEvents();
+            return this;
         },
 
-        bindEvents : function() {
-            addEvent(this.UI.hex, "keyup", function(event) {
-                if (Values.Utils.isHEX(this.value)) {
-                    Color.setColor( this.value );
-                    colorFormats.updateUIColors();
-                    colorFormats.UI.rgb.value = Color.rgb;
-                    colorFormats.UI.hsl.value = Color.hsl;
-                    return true;
-                }
-
-                colorFormats.UI.rgb.value = '';
-                colorFormats.UI.hsl.value = '';
-                return false;
-            });
-
-            addEvent(this.UI.rgb, "keyup", function(event) {
-                if (Values.Utils.isRGB(this.value)) {
-                    Color.setColor( this.value );
-                    colorFormats.updateUIColors();
-                    colorFormats.UI.hex.value = Color.hex;
-                    colorFormats.UI.hsl.value = Color.hsl;
-                    return true;
-                }
-
-                colorFormats.UI.hex.value = '';
-                colorFormats.UI.hsl.value = '';
-                return false;
-            });
-
-            addEvent(this.UI.hsl, "keyup", function(event) {
-                // if ( colorFormats.HSLRegExp.test(this.value) ) {
-                //     Color.setColor( this.value );
-                //     colorFormats.updateUIColors();
-                //     colorFormats.UI.hex.value = Color.getColor().hex;
-                //     colorFormats.UI.rgb.value = Color.getColor().rgb.text;
-                //     return true;
-                // }
-
-                // colorFormats.UI.hex.value = '';
-                // colorFormats.UI.rgb.value = '';
-                return false;
-            });
+        bindEvents : function bindEvents() {
+            addEvent(this.UI.hex, "keyup", this.hexKeyUpEvent);
+            addEvent(this.UI.rgb, "keyup", this.rgbKeyUpEvent);
+            return this;
         },
 
-        updateUIColors : function() {
+        hexKeyUpEvent : function hexKeyUpEvent() {
+            if (Values.Utils.isHEX(this.value)) {
+                Color.setColor( this.value );
+                colorFormats.updateUIColors();
+                colorFormats.UI.rgb.value = Color.rgb;
+                colorFormats.UI.hsl.value = Color.hsl;
+                return true;
+            }
+
+            colorFormats.UI.rgb.value = '';
+            colorFormats.UI.hsl.value = '';
+            return false;
+        },
+
+        rgbKeyUpEvent : function rgbKeyUpEvent () {
+            if (Values.Utils.isRGB(this.value)) {
+                Color.setColor( this.value );
+                colorFormats.updateUIColors();
+                colorFormats.UI.hex.value = Color.hex;
+                colorFormats.UI.hsl.value = Color.hsl;
+                return true;
+            }
+
+            colorFormats.UI.hex.value = '';
+            colorFormats.UI.hsl.value = '';
+            return false;
+        },
+
+        updateUIColors : function updateUIColors() {
             var bgClr   = Color.hex,
-                txtClr  = Color.brightness > 50 ?
-                    Color.lightness( -40 ) :
-                    Color.lightness( 40 ),
+                txtClr  = (Color.brightness > 50)
+                    ? Color.lightness( -40 )
+                    : Color.lightness( 40 ),
                 rgb     = txtClr._rgb.r+','+txtClr._rgb.g+','+txtClr._rgb.b;
 
             this.UI.body.style.backgroundColor = bgClr;
             this.UI.body.style.color = txtClr.hex;
             this.UI.wrapper.style.borderBottomColor = 'rgba('+rgb+', 0.5)';
-            this.UI._links.forEach(function(e, i) {
-                console.log(e)
-                e.style.color = txtClr.hex;
+            this.UI._links.forEach(function(el) {
+                el.style.color = txtClr.hex;
             });
+            bgClr = txtClr = rgb = null;
+            return this;
         }
     };
 
-    var addEvent = function (obj, type, fn) {
+    var addEvent = function addEvent(obj, type, fn) {
         if (obj.addEventListener) {
             obj.addEventListener(type, fn, false);
         } else if (obj.attachEvent) {
@@ -92,12 +85,7 @@
         }
     };
 
-    var triggerEvent = function (el, type) {
-        if ((el[type] || false) && typeof el[type] == 'function') {
-            el[type](el);
-        }
-    };
-
+    // init the thing
     colorFormats.init();
 
 })(window);
