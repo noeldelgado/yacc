fa06c47652ff2d934da7d94fe7d2d554a8f616d9
Noel Delgado <auhcsei@gmail.com>
2017-07-17 22:35:53 -0500

    update indentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,47 +1,46 @@
-module.exports = function(grunt) {
+module.exports = (grunt) => {
 
-    grunt.initConfig({
-        pkg : grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        compass : {
-            dist : {
-                options: {
-                    httpPath: '/',
-                    sassDir: 'source/sass',
-                    cssDir: 'source/stylesheets',
-                    imagesDir: 'images',
-                    outputStyle: 'compressed',
-                    relativeAssets: true
-                }
-            }
-        },
-
-        watch : {
-            css : {
-                files: ['source/sass/**/*.scss'],
-                tasks: ['compass'],
-                options: {
-                    livereload: true
-                }
-            },
-            js : {
-                files : ['source/javascripts/*.js'],
-                tasks: [],
-                options: {
-                    livereload: true
-                }
-            },
-            docs : {
-                files: ['index.html'],
-                tasks: [],
-                options: {
-                    livereload: true
-                }
-            }
+    compass: {
+      dist: {
+        options: {
+          httpPath: '/',
+          sassDir: 'source/sass',
+          cssDir: 'source/stylesheets',
+          imagesDir: 'images',
+          outputStyle: 'compressed',
+          relativeAssets: true
         }
-    });
+      }
+    },
 
-    require('load-grunt-tasks')(grunt);
-    grunt.registerTask('default', ['watch']);
+    watch: {
+      css: {
+        files: ['source/sass/**/*.scss'],
+        tasks: ['compass'],
+        options: {
+          livereload: true
+        }
+      },
+      js: {
+        files: ['source/javascripts/*.js'],
+        tasks: [],
+        options: {
+          livereload: true
+        }
+      },
+      docs: {
+        files: ['index.html'],
+        tasks: [],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
 
-}
+  require('load-grunt-tasks')(grunt);
+  grunt.registerTask('default', ['watch']);
+};
--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,136 +1,143 @@
-(function(global){
+(function(global) {
 
-    "use strict";
+  "use strict";
 
-    // instance of Values.js
-    global.Color = new Values('rgb(255, 0, 0)');
+  // instance of Values.js
+  global.Color = new Values('rgb(255, 0, 0)');
 
-    var colorFormats = {
-        UI : {
-            hex: document.querySelector('.js-hex'),
-            rgb: document.querySelector('.js-rgb'),
-            hsl: document.querySelector('.js-hsl'),
-            wrapper: document.querySelector('.main-container--inner'),
-            links: document.querySelectorAll('.footer a'),
-            body: document.body
-        },
+  var colorFormats = {
+    UI: {
+      hex: document.querySelector('.js-hex'),
+      rgb: document.querySelector('.js-rgb'),
+      hsl: document.querySelector('.js-hsl'),
+      wrapper: document.querySelector('.main-container--inner'),
+      links: document.querySelectorAll('.footer a'),
+      body: document.body
+    },
 
-        init : function init() {
-            this.UI._links = [].slice.call(this.UI.links, 0);
-            this.hash = global.location.hash;
-            this.bindEvents();
+    init: function init () {
+      this.UI._links = [].slice.call(this.UI.links, 0);
+      this.hash = global.location.hash;
+      this.bindEvents();
 
-            if (Values.Utils.isHEX(this.hash)) {
-                this.UI.hex.value = this.hash;
-                this.updateFromHex(this.hash);
-            } else global.location.hash = '';
+      if (Values.Utils.isHEX(this.hash)) {
+        this.UI.hex.value = this.hash;
+        this.updateFromHex(this.hash);
+      } else global.location.hash = '';
 
-            return this;
-        },
+      return this;
+    },
 
-        bindEvents : function bindEvents() {
-            addEvent(this.UI.hex, "keyup", this.hexKeyUpEvent);
-            addEvent(this.UI.rgb, "keyup", this.rgbKeyUpEvent);
-            addEvent(this.UI.hsl, "keyup", this.hslKeyUpEvent);
-            return this;
-        },
+    bindEvents: function bindEvents () {
+      addEvent(this.UI.hex, "keyup", this.hexKeyUpEvent);
+      addEvent(this.UI.rgb, "keyup", this.rgbKeyUpEvent);
+      addEvent(this.UI.hsl, "keyup", this.hslKeyUpEvent);
+
+      return this;
+    },
 
 
-        hexKeyUpEvent : function hexKeyUpEvent() {
-            if (Values.Utils.isHEX(this.value)) {
-                colorFormats.updateFromHex(this.value);
-                return true;
-            }
+    hexKeyUpEvent: function hexKeyUpEvent () {
+      if (Values.Utils.isHEX(this.value)) {
+        colorFormats.updateFromHex(this.value);
 
-            colorFormats.UI.rgb.value = '';
-            colorFormats.UI.hsl.value = '';
-            return false;
-        },
+        return true;
+      }
 
-        rgbKeyUpEvent : function rgbKeyUpEvent () {
-            if (Values.Utils.isRGB(this.value)) {
-                colorFormats.updateFromRGB(this.value);
-                return true;
-            }
+      colorFormats.UI.rgb.value = '';
+      colorFormats.UI.hsl.value = '';
+      return false;
+    },
 
-            colorFormats.UI.hex.value = '';
-            colorFormats.UI.hsl.value = '';
-            return false;
-        },
+    rgbKeyUpEvent: function rgbKeyUpEvent () {
+      if (Values.Utils.isRGB(this.value)) {
+        colorFormats.updateFromRGB(this.value);
 
-        hslKeyUpEvent : function hslKeyUpEvent() {
-            if (Values.Utils.isHSL(this.value)) {
-                colorFormats.updateFromHSL(this.value);
-                return this;
-            }
+        return true;
+      }
 
-            colorFormats.UI.hex.value = '';
-            colorFormats.UI.rgb.value = '';
-            return false;
-        },
+      colorFormats.UI.hex.value = '';
+      colorFormats.UI.hsl.value = '';
 
-        updateFromHex : function updateFromHex (value) {
-            Color.setColor(value);
-            this.updateUIColors();
-            this.UI.rgb.value = Color.rgbString();
-            this.UI.hsl.value = Color.hslString();
-            this.updateHash(Color.hex);
-        },
+      return false;
+    },
 
-        updateFromRGB : function updateFromRGB (value) {
-            Color.setColor(value);
-            this.updateUIColors();
-            this.UI.hex.value = Color.hexString();
-            this.UI.hsl.value = Color.hslString();
-            this.updateHash(Color.hex);
-        },
+    hslKeyUpEvent: function hslKeyUpEvent () {
+      if (Values.Utils.isHSL(this.value)) {
+        colorFormats.updateFromHSL(this.value);
 
-        updateFromHSL : function updateFromHSL(value) {
-            Color.setColor(value);
-            this.updateUIColors();
-            this.UI.hex.value = Color.hexString();
-            this.UI.rgb.value = Color.rgbString();
-            this.updateHash(Color.hex);
-        },
+        return this;
+      }
 
-        updateHash : function updateHash (value) {
-            this.hash = value;
-            global.location.hash = this.hash;
-        },
+      colorFormats.UI.hex.value = '';
+      colorFormats.UI.rgb.value = '';
 
-        updateUIColors : function updateUIColors() {
-            var isLight, bgClr, txtClr, borderClr;
+      return false;
+    },
 
-            isLight = (Color.getBrightness() > 50);
-            bgClr = Color.hexString();
-            txtClr = isLight ? Color.shade(50) : Color.tint(50);
-            borderClr = isLight ? Color.shade(10) : Color.tint(10);
+    updateFromHex: function updateFromHex (value) {
+      Color.setColor(value);
+      this.updateUIColors();
+      this.UI.rgb.value = Color.rgbString();
+      this.UI.hsl.value = Color.hslString();
+      this.updateHash(Color.hex);
+    },
 
-            this.UI.body.style.backgroundColor = bgClr;
-            this.UI.body.style.color = txtClr.hexString();
-            this.UI.wrapper.style.borderBottomColor = borderClr.hexString();
+    updateFromRGB: function updateFromRGB (value) {
+      Color.setColor(value);
+      this.updateUIColors();
+      this.UI.hex.value = Color.hexString();
+      this.UI.hsl.value = Color.hslString();
+      this.updateHash(Color.hex);
+    },
 
-            this.UI._links.forEach(function(el) {
-                el.style.color = txtClr.hex;
-            });
-            bgClr = txtClr = null;
-            return this;
-        }
-    };
+    updateFromHSL: function updateFromHSL (value) {
+      Color.setColor(value);
+      this.updateUIColors();
+      this.UI.hex.value = Color.hexString();
+      this.UI.rgb.value = Color.rgbString();
+      this.updateHash(Color.hex);
+    },
 
-    var addEvent = function addEvent(obj, type, fn) {
-        if (obj.addEventListener) {
-            obj.addEventListener(type, fn, false);
-        } else if (obj.attachEvent) {
-            obj['e' + type + fn] = fn;
-            obj[type + fn] = function() {
-                obj['e' + type + fn](window.event);
-            };
-            obj.attachEvent("on" + type, obj[type + fn]);
-        }
-    };
+    updateHash: function updateHash (value) {
+      this.hash = value;
+      global.location.hash = this.hash;
+    },
 
-    // init the thing
-    colorFormats.init();
+    updateUIColors: function updateUIColors () {
+      var isLight, bgClr, txtClr, borderClr;
 
+      isLight = (Color.getBrightness() > 50);
+      bgClr = Color.hexString();
+      txtClr = isLight ? Color.shade(50) : Color.tint(50);
+      borderClr = isLight ? Color.shade(10) : Color.tint(10);
+
+      this.UI.body.style.backgroundColor = bgClr;
+      this.UI.body.style.color = txtClr.hexString();
+      this.UI.wrapper.style.borderBottomColor = borderClr.hexString();
+
+      this.UI._links.forEach(function(el) {
+        el.style.color = txtClr.hex;
+      });
+      bgClr = txtClr = null;
+
+      return this;
+    }
+  };
+
+  var addEvent = function addEvent (obj, type, fn) {
+    if (obj.addEventListener) {
+      obj.addEventListener(type, fn, false);
+    }
+    else if (obj.attachEvent) {
+      obj['e' + type + fn] = fn;
+      obj[type + fn] = function() {
+        obj['e' + type + fn](window.event);
+      };
+      obj.attachEvent("on" + type, obj[type + fn]);
+    }
+  };
+
+  // init the thing
+  colorFormats.init();
 })(window);
